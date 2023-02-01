124a542c6eb882937d99e4b3a5fe92fcbccce3a1
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-01 12:24:44 +0900

    BaseStyleの調整

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 require_once(__DIR__ . '/layouts/header.php')
 ?>
 
-<main></main>
+<main class="l-main">
+  <div class="l-main__inner"></div>
+</main>
 
 <?php
 require_once(__DIR__ . '/layouts/footer.php')
