0f93aeea63db992bc51cd5e51f119872dccb3bff
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-01 12:24:18 +0900

    header,footerの作成

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,8 @@
-<footer></footer>
+<footer class="l-footer">
+  <div class="l-footer__inner">
+    <p class="l-footer__copy">&copy; 2023 シンプル商品管理 All rights reserved.</p>
+  </div>
+</footer>
 </body>
 
 </html>
