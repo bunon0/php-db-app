90f06ed8ecb82abe3d9c48def8f77dcdd2b4f665
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-01 13:40:06 +0900

    top-pageの追加とスタイル調整

--- index.php
+++ index.php
@@ -3,7 +3,17 @@ require_once(__DIR__ . '/layouts/header.php')
 ?>
 
 <main class="l-main">
-  <div class="l-main__inner"></div>
+  <div class="l-main__inner">
+    <div class="p-top">
+      <div class="p-top__contents">
+        <h1 class="p-top__title">商品管理アプリ</h1>
+        <p class="p-top__title-read">PHPとデータベースでシンプルな商品管理アプリ</p>
+        <div class="p-top__btn-wrapper">
+          <a href="./read.php" class="p-top__btn">商品一覧へ</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
 <?php
