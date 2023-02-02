c9244d15f9a324b58b0d17844a0396df19230d20
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-02 17:14:35 +0900

    商品名の検索機能の追加

--- read.php
+++ read.php
@@ -2,26 +2,39 @@
 require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/libs/FormSanitize.php');
 
-
 try {
-  // urlにパラメータの有無で処理を分ける
-  if (isset($_GET['sort'])) {
-    $get_sort = new FormSanitize($_GET['sort'], ENT_QUOTES, 'UTF-8');
-    $get_sort = $get_sort->sanitize();
-
-    // sortパラメータの値でsqlを変更する
-    if ($get_sort === 'desc') {
-      $sql = 'SELECT * FROM products ORDER BY updated_at DESC';
-    } else if ($get_sort === 'asc') {
-      $sql = 'SELECT * FROM products ORDER BY updated_at ASC';
-    } else {
-      $sql = 'SELECT * FROM products ORDER BY product_code ASC';
-    }
+  // sortパラメータが渡された時の処理
+  if (!empty($_GET['sort'])) {
+    $sort = new FormSanitize($_GET['sort'], ENT_QUOTES, 'UTF-8');
+    $sort = $sort->sanitize();
   } else {
-    $sql = 'SELECT * FROM products';
+    $sort = null;
   }
-  $stmt = $dbh->query($sql);
+
+  // keywordパラメータが渡された時の処理
+  if (!empty($_GET['keyword'])) {
+    $get_keyword = new FormSanitize($_GET['keyword'], ENT_QUOTES, 'UTF-8');
+    $get_keyword = $get_keyword->sanitize();
+  } else {
+    $get_keyword = null;
+  }
+  $keyword = "%{$get_keyword}%";
+
+  // sortパラメータの値でsqlを変更する
+  if ($sort === 'desc') {
+    $sql = 'SELECT * FROM products WHERE product_name LIKE :keyword ORDER BY updated_at DESC';
+  } else if ($sort === 'asc') {
+    $sql = 'SELECT * FROM products WHERE product_name LIKE :keyword ORDER BY updated_at ASC';
+  } else {
+    $sql = 'SELECT * FROM products WHERE product_name LIKE :keyword ORDER BY product_code ASC';
+  }
+
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindValue(':keyword', $keyword, PDO::PARAM_STR);
+  $stmt->execute();
   $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
   $dbh = null;
 } catch (PDOException $e) {
   exit('エラーが発生しました。' . $e->getMessage());
@@ -38,13 +51,19 @@ require_once(__DIR__ . '/layouts/header.php')
       <div class="p-read__contents">
         <h1 class="p-read__title">商品一覧</h1>
         <div class="p-read__widget">
-          <div class="p-read__widget-sort">
-            <a href="./read.php?sort=asc" class="p-read__widget-asc">
-              <i class="fa-solid fa-arrow-down-1-9"></i>
-            </a>
-            <a href="./read.php?sort=desc" class="p-read__widget-desc">
-              <i class="fa-solid fa-arrow-down-9-1"></i>
-            </a>
+          <div class="p-read__widget-filter">
+            <div class="p-read__widget-sort">
+              <a href="./read.php?sort=asc&keyword=<?= $get_keyword; ?>" class="p-read__widget-asc">
+                <i class="fa-solid fa-arrow-down-1-9"></i>
+              </a>
+              <a href="./read.php?sort=desc&keyword=<?= $get_keyword; ?>" class="p-read__widget-desc">
+                <i class="fa-solid fa-arrow-down-9-1"></i>
+              </a>
+            </div>
+            <form class="p-read__widget-search" action="./read.php" method="get">
+              <input type="hidden" name="sort" value="<?= $sort; ?>">
+              <input class="p-read__widget-input" type="text" name="keyword" value="<?= $get_keyword; ?>" placeholder="商品名で検索">
+            </form>
           </div>
         </div>
 
@@ -54,7 +73,7 @@ require_once(__DIR__ . '/layouts/header.php')
               <th class=" p-read__table-th">
                 商品コード
               </th>
-              <th class="p-read__table-th">
+              <th class="p-read__table-th" colspan="2">
                 商品名
               </th>
               <th class="p-read__table-th">
@@ -79,7 +98,7 @@ require_once(__DIR__ . '/layouts/header.php')
                 <td class='p-read__table-td' data-label="商品コード">
                   <?= $product['product_code']; ?>
                 </td>
-                <td class='p-read__table-td' data-label="商品名">
+                <td class='p-read__table-td' data-label="商品名" colspan="2">
                   <?= $product['product_name'] ?>
                 </td>
                 <td class='p-read__table-td' data-label="価格">
