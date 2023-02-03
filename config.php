ebb1f394eb54e1e8f26237fbff411b480e27b020
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-03 12:34:37 +0900

    データベース接続情報の変更

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <!-- DB接続テスト -->
 <?php
 $db_name = getenv('DB_NAME');
-$db_host = getenv('DB_HOST');
-$db_user = getenv('DB_USER');
-$db_pass = getenv('DB_PASS');
-
+$db_host = getenv('DB_HOSTNAME');
+$db_user = getenv('DB_USERNAME');
+$db_pass = getenv('DB_PASSWORD');
 
 try {
   $dbh = new PDO("mysql:host={$db_host};dbname={$db_name};", $db_user, $db_pass);
