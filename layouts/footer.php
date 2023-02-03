6e5f04bfc4b3cade31fdc1a4e38a7d7b4f16f1ad
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-03 14:25:37 +0900

    削除確認でキャンセルしても削除されてしまう問題を修正

--- layouts/footer.php
+++ layouts/footer.php
@@ -3,6 +3,14 @@
     <p class="l-footer__copy">&copy; 2023 シンプル商品管理 All rights reserved.</p>
   </div>
 </footer>
+<script>
+  const confirmHandle = () => {
+    const flag = confirm('本当に削除しますか？')
+    if (!flag) {
+      return false;
+    }
+  }
+</script>
 </body>
 
 </html>
--- read.php
+++ read.php
@@ -138,7 +138,7 @@ require_once(__DIR__ . '/layouts/header.php')
                   <a class="p-read__btn-edit" href="./update.php?id=<?= $product['id'] ?>">編集</a>
                 </td>
                 <td class='p-read__table-td'>
-                  <a class="p-read__btn-delete" href="./delete.php?id=<?= $product['id'] ?>" onclick="confirm('本当に削除してもよろしいですか？')">削除</a>
+                  <a class="p-read__btn-delete" href="./delete.php?id=<?= $product['id'] ?>" onclick="return confirmHandle()">削除</a>
                 </td>
               </tr>
             <?php endforeach; ?>
