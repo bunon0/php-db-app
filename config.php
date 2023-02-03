c8b59d4bf56ae9063e246d4f8fcfb05dd7cece2b
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-03 12:59:54 +0900

    pdoクラスに文字コードの設定を追加

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $db_user = getenv('DB_USERNAME');
 $db_pass = getenv('DB_PASSWORD');
 
 try {
-  $dbh = new PDO("mysql:host={$db_host};dbname={$db_name};", $db_user, $db_pass);
+  $dbh = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8mb4", $db_user, $db_pass);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   exit('エラーが発生しました。' . $e->getMessage());
